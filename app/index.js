96fdaa5b9c3111f80601e671549ab4e909aaea3b
Ben Ratner <ben.m.ratner@gmail.com>
2016-12-06 20:12:13 -0500

    finish v1 of site

--- app/index.js
+++ app/index.js
@@ -10,6 +10,15 @@ $(document).ready(function() {
         interval: 7500
     })
 
+    $('#bs-example-navbar-collapse-1').on('show.bs.collapse', function() {
+        $('.nav-pills').addClass('nav-stacked');
+    });
+
+    //Unstack menu when not collapsed
+    $('#bs-example-navbar-collapse-1').on('hide.bs.collapse', function() {
+        $('.nav-pills').removeClass('nav-stacked');
+    });
+
 
 
 });
